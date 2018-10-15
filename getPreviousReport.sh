3054199f1d5cf0ccddc29d752cb495c13f7d8053
Brice Vandeputte <brice.vandeputte@orange.com>
2018-10-15 14:22:52 +0200

    getPreviousReport fix negative cases

--- getPreviousReport.sh
+++ getPreviousReport.sh
@@ -3,8 +3,8 @@ PREVIOUS_DOCS=./previous_docs
 echo " * get previous report"
 rm -rf ${PREVIOUS_DOCS} ||true
 git clone https://github.com/geokrety/geokrety-website-qa.git --branch gh-pages --single-branch ${PREVIOUS_DOCS}
-rm -rf ${PREVIOUS_DOCS}/.git || true
-cp -rf ${PREVIOUS_DOCS}/boly38 docs/ 2>/dev/null || true
-cp -rf ${PREVIOUS_DOCS}/master docs/ 2>/dev/null || true
+rm -rf ${PREVIOUS_DOCS}/.git || echo "not a git repo"
+cp -rf ${PREVIOUS_DOCS}/boly38 docs/ 2>/dev/null || echo "no previous docs for boly38"
+cp -rf ${PREVIOUS_DOCS}/master docs/ 2>/dev/null || echo "no previous docs for master"
 echo " * imported previous reports:"
 ls -d docs/*/
